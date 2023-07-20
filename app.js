d20ad3ac2f7db5ca6524a2035d456fc445c298d2
Rakin Brown <rakinbrown38@gmail.com>
2023-07-20 16:41:45 -0500

    JavaScript Variables Complete

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
+const firstName = "Rakin"
+lastName = "Brown"
+age = 32;
 
 // Exercise 3
+let language = 'JavaScript'
+let createdYear = 1995
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStoc = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
